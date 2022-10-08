1ac4723530ab0d297afe64fb7ba083deaf40f46e
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-10-08 13:49:46 +0200

    feat(trpc): superjson

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,9 +1,13 @@
 import { initTRPC } from "@trpc/server";
 
+import superjson from "superjson";
+
 import { brandRouter } from "./brand/router";
 import { discRouter } from "./disc/router";
 
-const t = initTRPC.create();
+const t = initTRPC.create({
+  transformer: superjson,
+});
 
 export const appRouter = t.router({
   brand: brandRouter,
