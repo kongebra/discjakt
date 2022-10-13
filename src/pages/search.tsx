51a480a4712bdb357f4a1439f5476ed74f9796c8
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-10-13 09:16:20 +0200

    feat: add fb domain

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,18 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "next/router";
 import React from "react";
-import Container from "src/components/Container";
-import DiscFeaturedItem from "src/components/DiscFeaturedItem";
-import Heading from "src/components/Heading";
-import Input from "src/components/Input";
-import SelectDiscSort from "src/components/SelectDiscSort";
+import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
+
+import {
+  Container,
+  DiscFeaturedItem,
+  SelectDiscSort,
+  Heading,
+} from "src/components";
+
 import config from "src/config";
+
 import useSortDiscs from "src/hooks/use-sort-discs";
+
 import { DiscDetails } from "src/types/prisma";
+
 import { getQueryStringValue } from "src/utils/query";
 
 const fetchSearch = async (query?: string) => {
-  const url = `${config.apiUrl}/api/discs/search?q=${query || ""}`;
+  const url = `${config.baseUrl}/api/discs/search?q=${query || ""}`;
   const response = await fetch(url);
   const data = await response.json();
 
